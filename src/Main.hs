b0a52bd5b0c0a5df70c911dec0655884dfa412e2
Heorhii Sanchenko <jorhesancho@gmail.com>
2024-01-13 20:52:59 +0200

    move run

--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,7 @@
 module Main where
 
-import AST
-import Data.Map qualified
-import Lang
-import Parse
-import ProgState (ProgState (ProgState))
+import Run
 import System.Environment
-import System.IO
-
-runFile :: FilePath -> IO ()
-runFile fileName = do
-  progTree <- parseOrThrow parseProg <$> readFile fileName
-  result <- runProgram progTree $ ProgState Data.Map.empty Data.Map.empty
-  print result
-
-runBaseFile :: String -> IO ()
-runBaseFile fileName = runFile ("../text/" ++ fileName ++ ".txt")
 
 main :: IO ()
 main = do
---
+++ src/Run.hs
@@ -0,0 +1,18 @@
+module Run where
+
+import AST
+import Data.Map qualified
+import Lang
+import Parse
+import ProgState (ProgState (ProgState))
+import System.Environment
+import System.IO
+
+runFile :: FilePath -> IO ()
+runFile fileName = do
+  progTree <- parseOrThrow parseProg <$> readFile fileName
+  result <- runProgram progTree $ ProgState Data.Map.empty Data.Map.empty
+  print result
+
+runBaseFile :: String -> IO ()
+runBaseFile fileName = runFile ("../text/" ++ fileName ++ ".txt")
